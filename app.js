04ee37dd3601178dcacf5d4d4ab165d54a108c03
Zack Klinger <zklinger@gmail.com>
2016-04-12 20:03:46 -0700

    CLEAN and doc the base PomoTimer and Controller

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ angular.module('pomoApp', [])
 .run(function ($rootScope) {
   $rootScope._ = window._;
 })
-//.constant('moment', window.moment)
 // Main Controller
 .controller('MainController', function($scope, $interval) {
   var vm = this;
   
+  // Create a PomoTimer
   vm.pomo = new PomoTimer();
   
+  // PomoTimer constructor
   function PomoTimer() {
     this.work = {
       minutes: 0,
@@ -26,43 +27,50 @@ angular.module('pomoApp', [])
       secondsStr: '00',
       inputMinutes: 1
     };
-
+    // Create a Rest Timer
     this.rest.timer = new Timer(this.rest);
-
+    // Create a Work Timer and pass it the Rest Timer
     this.work.timer = new Timer(this.work, this.rest);
   }
-  
+  // Timer constructor
   function Timer(timer, next) {
-    this.stop;
+    this._stop;  // promise from $interval timer
     
+    // METHODS
     this.stopTimer = stopTimer;
     this.startTimer = startTimer;
     this.addMinutes = addMinutes;
     this.subMinutes = subMinutes;
     
+    // Cancel the $interval and set its promise to 'undefined'
     function stopTimer() {
-      if (angular.isDefined(this.stop)) {
-        $interval.cancel(this.stop);
-        this.stop = undefined;
+      if (angular.isDefined(this._stop)) {
+        $interval.cancel(this._stop);
+        this._stop = undefined;
       }
     }
-    
+    // Start the $interval and assing its promise to _stop
     function startTimer(next) {
       // Return immediately if timer is running
-      if (angular.isDefined(this.stop)) return;
-      // Assign stop the promise
-      this.stop = $interval(function() {
+      if (angular.isDefined(this._stop)) return;
+      // Assign _stop the promise returned from $interval
+      this._stop = $interval(function() {
+        // If there is a next timer, start it at 1 second left on this timer
         if (next && timer.minutes === 0 && timer.seconds === 1) {
           next.timer.startTimer();
         }
+        // If timer runs out, call stopTimer() to unassign 'stop' properly
         if (timer.seconds === 0 && timer.minutes === 0) {
           stopTimer();
+        // Else, if we hit zero seconds with minutes left, decrement minutes, seconds
         } else if (timer.seconds === 0) {
           --timer.minutes;
           timer.seconds = 59;
+        // Else, decrement seconds
         } else {
           --timer.seconds;
         }
+        // Set the string for seconds to pad a zero if needed
         timer.secondsStr = timer.seconds.toString().length === 1 ? '0' + timer.seconds : timer.seconds;
       }, 1000);
     }
